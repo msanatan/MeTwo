6f9734a1d514dce90264cd98894d7daf64b1c607
Marcus Sanatan <msanatan@gmail.com>
2021-06-16 00:34:16 -0400

    Add script to collect gem
    This script allows a player or duplicate to collect gems.
    
    It's added to the player object and duplicate prefab.
    
    Of course, to work we also added a collider for the gem

---
+++ Assets/Scripts/CollectGem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectGem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Gem")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
