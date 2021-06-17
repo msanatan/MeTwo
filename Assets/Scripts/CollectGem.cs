06a07cee0384f354cb59fb654f8e497a306f4eab
Marcus Sanatan <msanatan@gmail.com>
2021-06-17 14:00:05 -0400

    Transition scene when collecting gem
    When we get the gem the next level loads

--- Assets/Scripts/CollectGem.cs
+++ Assets/Scripts/CollectGem.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CollectGem : MonoBehaviour
 {
+    private SceneLoader sceneLoader;
+
+    private void Awake()
+    {
+        sceneLoader = new SceneLoader();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Gem")
         {
             Destroy(collision.gameObject);
+            sceneLoader.LoadNextScene();
         }
     }
 }
