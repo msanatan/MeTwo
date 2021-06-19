5bcb457e8cf2ad69b86607db0b0c6766f99ea1b5
Marcus Sanatan <msanatan@gmail.com>
2021-06-19 16:32:11 -0400

    Get Scene Loader from object instead
    This should fix the warning of not calling "new".
    
    I guess we had to make a game object for Unity to be happy anyway

--- Assets/Scripts/CollectGem.cs
+++ Assets/Scripts/CollectGem.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class CollectGem : MonoBehaviour
 {
-    private SceneLoader sceneLoader;
-
-    private void Awake()
-    {
-        sceneLoader = new SceneLoader();
-    }
+    [SerializeField] GameObject sceneLoader;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Gem")
         {
             Destroy(collision.gameObject);
-            sceneLoader.LoadNextScene();
+            sceneLoader.GetComponent<SceneLoader>().LoadNextScene();
         }
     }
 }
